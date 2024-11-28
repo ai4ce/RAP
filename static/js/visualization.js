48ccea60554b1788710e98a785ff5fb425a56acc
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2024-11-28 09:47:36 +0800

    1

--- static/js/visualization.js
+++ static/js/visualization.js
@@ -1,5 +1,3 @@
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.170.0/three.module.js';
-
 // 初始化场景
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / 600, 0.1, 1000);
